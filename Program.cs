8bfe31ab4f2d32885e4e4f7d59f145e4194a6865
nbarsou <71356372+nbarsou@users.noreply.github.com>
2021-01-26 22:12:06 -0600

    Added Break time tracking

--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@ namespace WorkTracker
         {
             Console.Clear();
             System.Console.WriteLine("Welcome to TimeTrack");
+
             //Main menu UI 
             do{
-                System.Console.WriteLine("Press <ESC> to exit .....");
+                System.Console.WriteLine("Press [T] for TimeTrack");
                 switch (Console.ReadKey().Key)
                 {
                     case ConsoleKey.T: 
@@ -27,12 +28,14 @@ namespace WorkTracker
                         break; 
                     
                 }
+                System.Console.WriteLine("Press <ESC> to exit .....");
             }while(Console.ReadKey().Key != ConsoleKey.Escape);
         } 
 
         static void timeTrack(){ 
             // Start the TimeTrack 
             TimeSpan totalTime = TimeSpan.Zero;
+            TimeSpan totalBreakTime = TimeSpan.Zero; 
             DateTime startTime = DateTime.Now; 
             Console.Clear();
             System.Console.WriteLine("TimeTrack Started at: {0:d} at {0:t}", startTime);
@@ -46,19 +49,29 @@ namespace WorkTracker
                     System.Console.WriteLine("TimeTrack Stoped at: {0:d} at {0:t}", DateTime.Now);
                     totalTime += DateTime.Now.Subtract(startTime);
                     System.Console.WriteLine("Total Time at: " + totalTime);
+                    startTime = DateTime.Now; 
                     
                     // Resume the TimeTrack by reseting th startTime 
                     System.Console.WriteLine("Press [P] to Unpause the TimeTrack");
                     while(Console.ReadKey().Key != ConsoleKey.P){}
                     Console.Clear();
+                    totalBreakTime += DateTime.Now.Subtract(startTime);
+                    System.Console.WriteLine("Total Break Time at: " + totalBreakTime);
                     System.Console.WriteLine("TimeTrack Resumed at: {0:d} at {0:t}", DateTime.Now);
                     System.Console.WriteLine("[ENTER] to Stop       [P] to Pause");
                     startTime = DateTime.Now;
                 }
             }
             // Get the Total Time 
+            Console.Clear();
             totalTime += DateTime.Now.Subtract(startTime);
-            System.Console.WriteLine("Total Time Spent Workin : " + totalTime);
+            System.Console.WriteLine("Total Time Spent Working : " + totalTime);
+            System.Console.WriteLine("Total Time Spent on Break: " + totalBreakTime); 
+            if(totalBreakTime > totalTime){
+                Console.WriteLine("You were more time on break, try to work tomorrow more!");
+            }else{
+                Console.WriteLine("Good job on today, keep it up");
+            }
              
         }
     }
